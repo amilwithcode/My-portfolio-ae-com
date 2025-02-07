38131d3864125a1d7a118850b0dbbce1c4236841
User <mywithcontact@gmail.com>
2025-02-07 11:57:46 +0400

    edit:robot.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,37 +1,44 @@
-import type { MetadataRoute } from "next";
 
-// eslint-disable-next-line
-type Robots = {
+interface Robots {
   rules:
-    | {
-        userAgent?: string | string[];
-        allow?: string | string[];
-        disallow?: string | string[];
-        crawlDelay?: number;
-      }
-    | Array<{
-        userAgent: string | string[];
-        allow?: string | string[];
-        disallow?: string | string[];
-        crawlDelay?: number;
-      }>;
+  | {
+    userAgent?: string | string[];
+    allow?: string | string[];
+    disallow?: string | string[];
+  }
+  | Array<{
+    userAgent: string | string[];
+    allow?: string | string[];
+    disallow?: string | string[];
+  }>;
   sitemap?: string | string[];
   host?: string;
 };
 
-export default function robots(): MetadataRoute.Robots {
+export default function robots(): Robots {
   return {
     rules: [
       {
-        userAgent: "Googlebot",
-        allow: ["/"],
-        disallow: "/private/",
+        userAgent: "*",
+        allow: ["/src/app/[locale]/(root)/*"],
+        disallow: [
+          "/private/src/assets/*",
+          "/private/src/mesagges/*",
+          "/private/src/components/*",
+          "/private/src/error/*",
+          "/private/src/lib/*",
+          "/private/src/style/*",
+          "/private/src/ui/*",
+          "/private/src/i18n",
+          "/private/src/middleware",
+          "/private/src/navigation",
+          "/private//src/public/*",
+          "/private/src/robots.txt"
+        ],
       },
-      {
-        userAgent: ["Applebot", "Bingbot"],
-        disallow: ["/"],
-      },
+
     ],
-    sitemap: "my-portfolio.ae.com",
+    sitemap: "my-portfolio-ae.web.app.xml",
+    host:"my-portfolio-ae.web.app"
   };
 }
