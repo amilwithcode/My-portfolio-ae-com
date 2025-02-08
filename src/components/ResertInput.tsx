c2cefd090a9864e1aa345d1f1e0fe91dbdb1113d
User <mywithcontact@gmail.com>
2025-02-08 19:23:55 +0400

    add:lsngusges edited and reset emal

--- src/components/ResertInput.tsx
+++ src/components/ResertInput.tsx
@@ -1,26 +1,56 @@
-import React from 'react'
-// import { useTranslations } from "next-intl";
+import { toast } from "react-toastify";
+import { useState } from "react";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { useTranslations } from "next-intl";
+
 
 export function ResertInput() {
-    return (
-        <div className=' mx-1  py-36  dark:bg-black  dark:text-white '>
-            <label
-                htmlFor="email"
-                className='text-sm font-bold cursor-pointer'>Resert pasword
-            </label>
-            <div className="input flex  gap-2">
+    const [email, setEmail] = useState('')
+    const auth = getAuth();
+    const t = useTranslations("ResertPassworPage")
 
-                <input
-                    id='email'
-                    type="email"
-                    className="border border-gray-300 rounded-md w-full p-2"
-                    placeholder="Email"
-                    required
-                />
-                <button className="bg-blue-500 text-white px-3 py-2 rounded-md ml-2">
-                    Reset
-                </button>
-            </div>
+    const ResetPasswordEmail = async (e) => {
+        e.preventDefault()
+        try {
+            await sendPasswordResetEmail(auth, email)
+
+
+        } catch (error) {
+            console.log(error);
+            toast.error('You write to again this email!', {
+                position: "top-center"
+            })
+            console.log(error);
+            // ..
+
+        };
+    }
+
+    return (
+        <div className='flex-col items-center justify-center mx-1  py-36  dark:bg-black  dark:text-white '>
+            <h1 className="flex justify-center items-center text-center text-xl font-bold cursor-pointer">{t("title")}</h1>
+            <form action="">
+
+                <label
+                    htmlFor="email"
+                    className='text-sm font-bold cursor-pointer'
+                >
+                    {t("email")}
+                </label>
+                <div className="input flex  gap-2">
+
+                    <input
+                        id='email'
+                        type="email"
+                        className="border border-gray-300 rounded-md w-full p-2"
+                        placeholder="Email"
+                        required
+                    />
+                    <button className="bg-blue-500 text-white px-3 py-2 rounded-md ml-2" onClick={ResetPasswordEmail}>
+                        {t("button")}
+                    </button>
+                </div>
+            </form>
         </div>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}', 
-    './src/components/**/*.{js,ts,jsx,tsx}', 
-    './src/app/**/*.{js,ts,jsx,tsx}', 
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode:'class',
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
@@ -13,6 +13,20 @@ module.exports = {
       },
       colors: {
         aqua: '#0000',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        slideIn: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out',
+        slideIn: 'slideIn 0.5s ease-out',
       },
     }
   },
